5aae91798e3ba4f25aca8751189f879745c76fc4
Chris <ChrisDick@users.noreply.github.com>
2025-05-25 23:52:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,18 @@
 from app_components import clear_background
 
-
 class TildaGone:
     def __init__(self):
-        self.fade = 20
+        self.fade = 500
 
     def update(self, delta):
-        self.fade = self.fade - (delta / 100)
+        self.fade = (self.fade - (delta / 20))
         if self.fade < 0:
-            self.fade = 0
+            self.fade = 500
 
     def draw(self, ctx):
         clear_background(ctx)
-        ctx.save()
         ctx.font_size = self.fade
-        ctx.text_align = ctx.LEFT
-        ctx.text_baseline = ctx.TOP
-        ctx.rgb()
-        ctx.text("~")
-        ctx.restore()
+        ctx.rgb(*(0.5, 0.5, 1.0)).move_to(0-(self.fade/4), 0 + (self.fade /4)).text("~")
 
 
 __Background__ = TildaGone
